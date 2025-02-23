23cb29a99a6ae1a15f4bd26987f1ff38bb4b9213
JFogotcheck <fogotcheck@live.com>
2025-02-23 11:08:12 +0300

    Initial

---
+++ AppMain/src/main.cpp
@@ -0,0 +1,27 @@
+#include "main.h"
+
+void MainThr(__attribute__((unused)) void *arg);
+void TestThr(void *arg);
+
+int main(void)
+{
+	system_init();
+	BaseType_t ret = xTaskCreate(MainThr, "MainTask", 500, NULL, 1, NULL);
+	if (ret != pdPASS) {
+		Error_Handler();
+	}
+	vTaskStartScheduler();
+	Error_Handler();
+}
+
+void MainThr(__attribute__((unused)) void *arg)
+{
+	if (osKernelInitialize()) {
+		Error_Handler();
+	}
+
+	while (1) {
+		HAL_GPIO_TogglePin(LD1_GPIO_Port, LD1_Pin);
+		vTaskDelay(500);
+	}
+}
